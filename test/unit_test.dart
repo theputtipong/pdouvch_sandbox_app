d40d690339a22c3c7a41d2d921a2717fa1be809f
Mr.Puttipong Doungvichai <puttipong.dou@gmail.com>
2024-07-10 11:12:52 +0700

    update

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,15 +1,37 @@
-// This is an example unit test.
-//
-// A unit test tests a single function, method, or class. To learn more about
-// writing unit tests, visit
-// https://flutter.dev/docs/cookbook/testing/unit/introduction
+// ignore_for_file: constant_identifier_names
 
+import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Plus Operator', () {
-    test('should add two numbers together', () {
-      expect(1 + 1, 2);
+  TestWidgetsFlutterBinding.ensureInitialized();
+  group('JSONS Load Test', () {
+    test('should load JSONS data from assets ${TypeJson.cm} : ${JsonFile.choices}', () async {
+      final data = await _loadJsonData(product: TypeJson.cm, filename: JsonFile.choices);
+      expect(data, isNotEmpty);
     });
   });
+}
+
+enum TypeJson { cm, pm }
+
+enum JsonFile {
+  choices,
+  inspection_detail,
+  list_inspection,
+  list_rfi,
+  list_submittal,
+  list_wo,
+  rfi_detail,
+  submittal_detail,
+  wo_detail,
+  document_detail,
+  documents
+}
+
+Future<List<dynamic>> _loadJsonData({required TypeJson product, required JsonFile filename}) async {
+  final String response = await rootBundle.loadString('assets/jsons/${product.name}/${filename.name}.json');
+  final data = await json.decode(response);
+  return data;
 }
