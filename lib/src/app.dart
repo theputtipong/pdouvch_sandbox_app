d3c9223c693b75dffa5fd704cd15783ef86e3857
Mr.Puttipong Doungvichai <puttipong.dou@gmail.com>
2024-07-16 12:50:37 +0700

    add download file

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -15,38 +15,42 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListenableBuilder(
-      listenable: settingsController,
-      builder: (BuildContext context, Widget? child) {
-        return RepositoryProvider(
-          create: (context) => MapController(),
-          child: MaterialApp(
-            localizationsDelegates: const [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: const [Locale('en', '')],
-            theme: ThemeData(),
-            darkTheme: ThemeData.dark(),
-            themeMode: settingsController.themeMode,
-            onGenerateRoute: (RouteSettings routeSettings) {
-              return MaterialPageRoute<void>(
-                settings: routeSettings,
-                builder: (BuildContext context) {
-                  switch (routeSettings.name) {
-                    case SettingsView.routeName:
-                      return SettingsView(controller: settingsController);
-                    default:
-                      return const MapView();
-                  }
-                },
-              );
-            },
-          ),
-        );
-      },
+    return SafeArea(
+      top: false,
+      child: ListenableBuilder(
+        listenable: settingsController,
+        builder: (BuildContext context, Widget? child) {
+          return RepositoryProvider(
+            create: (context) => MapController(),
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              localizationsDelegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: const [Locale('en', '')],
+              theme: ThemeData(),
+              darkTheme: ThemeData.dark(),
+              themeMode: settingsController.themeMode,
+              onGenerateRoute: (RouteSettings routeSettings) {
+                return MaterialPageRoute<void>(
+                  settings: routeSettings,
+                  builder: (BuildContext context) {
+                    switch (routeSettings.name) {
+                      case SettingsView.routeName:
+                        return SettingsView(controller: settingsController);
+                      default:
+                        return const MapView();
+                    }
+                  },
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/src/feat/map/map_view.dart
+++ lib/src/feat/map/map_view.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'cubits/cubits.dart';
 import 'map_ctl.dart';
 
@@ -12,9 +16,6 @@ class MapView extends StatelessWidget {
     final mapController = RepositoryProvider.of<MapController>(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map View'),
-      ),
       body: MultiBlocProvider(
         providers: [
           BlocProvider.value(value: mapController.locationCubit),
@@ -31,57 +32,18 @@ class MapView extends StatelessWidget {
             return Stack(
               children: [
                 GoogleMap(
-                  onMapCreated: (GoogleMapController controller) {
-                    locationCubit.setGoogleMapController(controller: controller);
-                  },
-                  initialCameraPosition: CameraPosition(
-                    target: state.currentLocation ?? const LatLng(0, 0),
-                    zoom: 14.0,
-                  ),
-                  polylines: {
-                    Polyline(
-                      polylineId: const PolylineId('route'),
-                      points: state.routePoints,
-                      color: Colors.blue,
-                      width: 5,
-                    ),
-                  },
-                  myLocationEnabled: true,
-                ),
-                Positioned(
-                  bottom: 16,
-                  left: 16,
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      if (state.isTracking) {
-                        locationCubit.stopTracking();
-                      } else {
-                        locationCubit.startTracking();
-                      }
-                    },
-                    tooltip: state.isTracking ? 'Stop Tracking' : 'Start Tracking',
-                    child: Icon(state.isTracking ? Icons.stop : Icons.play_arrow),
-                  ),
-                ),
-                Positioned(
-                  bottom: 80,
-                  left: 16,
-                  child: BlocBuilder<TimerCubit, TimerState>(
-                    builder: (context, timerState) {
-                      return FloatingActionButton(
-                        onPressed: () {
-                          final timerCubit = context.read<TimerCubit>();
-                          if (timerCubit.state.duration.inSeconds % 2 == 0) {
-                            timerCubit.stopTimer();
-                          } else {
-                            timerCubit.startTimer();
-                          }
-                        },
-                        tooltip: 'Time: ${timerState.duration.inSeconds} seconds',
-                        child: const Icon(Icons.timer),
-                      );
-                    },
-                  ),
+                  onMapCreated: (GoogleMapController controller) =>
+                      locationCubit.setGoogleMapController(controller: controller),
+                  initialCameraPosition:
+                      CameraPosition(target: state.currentLocation ?? const LatLng(0, 0), zoom: 14.0),
+                  myLocationEnabled: false,
+                  myLocationButtonEnabled: false,
+                  // markers: <Marker>[
+                  //   Marker(
+                  //     markerId: const MarkerId('current'),
+                  //     position: state.currentLocation ?? const LatLng(0, 0),
+                  //   ),
+                  // ].toSet(),
                 ),
               ],
             );
@@ -89,5 +51,41 @@ class MapView extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<bool> downloadFile({
+    required String urlDownload,
+    required String fileName,
+  }) async {
+    bool check = await _checkPermission();
+    if (!check) return false;
+    var dir = await getApplicationDocumentsDirectory();
+    String savePath = "${dir.path}/$fileName";
+    try {
+      var request = await http.Client().get(Uri.parse(urlDownload));
+      var bytes = request.bodyBytes;
+      File file = File(savePath);
+      await file.writeAsBytes(bytes);
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> _checkPermission() async {
+    if (Platform.isAndroid || Platform.isIOS) {
+      var status = await Permission.storage.status;
+      if (status.isGranted) {
+        return true;
+      } else if (status.isPermanentlyDenied) {
+        bool permission = await openAppSettings();
+        return permission;
+      } else {
+        bool permission = await Permission.storage.request() == PermissionStatus.granted;
+        return permission;
+      }
+    }
+    return true;
   }
 }
