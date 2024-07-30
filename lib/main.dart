85f5ddc62531bd6b74322106c98d97aec4ea49f5
SWD Puttipongd <puttipong.d@swiftdynamics.co.th>
2024-07-30 10:21:11 +0700

    clear lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
-import 'src/settings/settings.dart';
 
 void main() async {
-  final settingsController = SettingsCtl(SettingsServ());
-
-  await settingsController.loadSettings();
-
-  runApp(MyApp(settingsController: settingsController));
+  runApp(const MyApp());
 }
