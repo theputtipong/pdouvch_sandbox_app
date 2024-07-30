9958886dfaa5ef7cc57dcd544e248aff32c389fd
SWD Puttipongd <puttipong.d@swiftdynamics.co.th>
2024-07-30 14:51:23 +0700

    update

--- android/app/src/main/kotlin/com/pdouvch/sandboxflutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/pdouvch/sandboxflutter/MainActivity.kt
@@ -1,5 +1,40 @@
 package com.pdouvch.sandboxflutter
 
+import android.provider.Settings.Secure
+import android.provider.Settings.Secure.*
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "pdouvch.com/channel"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                when (call.method) {
+                    "getDeviceId" -> {
+                        val id = getAndroidDeviceId()
+                        if (id != null) {
+                            result.success(id)
+                        } else {
+                            result.error("UNAVAILABLE", "Device ID not available.", null)
+                        }
+                    }
+
+                    else -> {
+                        result.notImplemented()
+                    }
+                }
+            }
+    }
+
+    fun getAndroidDeviceId(): String? {
+        return try {
+            getString(/* resolver = */ contentResolver, /* name = */ ANDROID_ID)
+        } catch (e: Exception) {
+            "error_getting_device_id"
+        }
+    }
+}
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({
@@ -11,6 +12,16 @@ class MyApp extends StatelessWidget {
       bottom: true,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
+        title: 'Pdouvch Sandbox Flutter',
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('en'), // English
+          Locale('th'), // Thailand
+        ],
       ),
     );
   }
